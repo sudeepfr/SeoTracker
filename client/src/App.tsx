1809620bf8a0fa64fe1d466275b93376793dd224
sudeepfr <sudeepverma764@gmail.com>
2026-07-02 10:41:16 +0530

    added loading component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,13 +11,14 @@ import RankTracker from "./pages/RankTracker";
 import RankDetail from "./pages/RankDetail";
 import { Toaster } from "react-hot-toast";
 import { useApp } from "./context/appContext";
+import Loading from "./components/Loading";
 
 export default function App() {
     const {user,loading}=useApp();
     const location = useLocation();
     
     const hideNavbar = ["/login", "/register"].includes(location.pathname);
-
+     if(loading) return <Loading/>
     return (
         <>
             <Toaster />
